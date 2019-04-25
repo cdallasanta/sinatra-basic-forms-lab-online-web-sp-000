32af0b6854c0ae8327eaf1f7283a9e6eb816ed58
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-25 04:17:47 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
-  attr_accessor :name, :months_old, :breed
+  attr_accessor :name, :breed, :months_old
 
-  def initialize(name, age, breed)
+  def initialize(name, breed, age)
     @name = name
     @months_old = age
     @breed = breed
